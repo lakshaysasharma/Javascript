38b490935b5b7aaa836769ed5933e2578dec8b8f
Lakshay Sharma <lakshaysasharma@gmail.com>
2026-03-26 12:33:16 +0530

    math

--- basics/06_nums_and_math.js
+++ basics/06_nums_and_math.js
@@ -19,13 +19,25 @@ const hundreds = 1000000
 
 //  --------------------- Maths -----------------------
 
-console.log(Math);
-console.log(Math.abs(-4));  // change the given value to positive, but not positive to negative
-console.log(Math.round(4.6));  // 5
-console.log(Math.ceil(4.2));  // 5
-console.log(Math.floor(4.2));  // 4
+// console.log(Math);
+// console.log(Math.abs(-4));  // change the given value to positive, but not positive to negative
+// console.log(Math.round(4.6));  // 5
+// console.log(Math.ceil(4.2));  // 5
+// console.log(Math.floor(4.2));  // 4
 
-console.log(Math.max(2, 4, 5, 1));  // gives the max value
-console.log(Math.min(2, 4, 5, 1));
-console.log(Math.min(2));  // gives 2
+// console.log(Math.max(2, 4, 5, 1));  // gives the max value
+// console.log(Math.min(2, 4, 5, 1));
+// console.log(Math.min(2));  // gives 2
+
+
+// console.log(Math.random());  // generates a float number between 0 and 1(exclusive)
+// console.log(Math.random()*10);
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
 
