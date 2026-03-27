fe4e1a5fa68baf5a37b750cf8f06b2dcf730a9c3
Lakshay Sharma <lakshaysasharma@gmail.com>
2026-03-27 12:59:14 +0530

    arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,16 +1,16 @@
 // Array
 const myArr = [0, 1, 2, 3, 4, 5]
-// console.log(myArr[1]);
+console.log(myArr[1]);
 const myHeros = ["ironman", "batman"]
 const myArr2 = new Array(1, 2, 3, 4)
 const arr = [5]
 const arr2 = new Array(5)  
-// console.log(arr);
-// console.log(arr2);  // [ <5 empty items> ]
+console.log(arr);
+console.log(arr2);  // [ <5 empty items> ]
 arr2.push(1, 2, 3, 4, 5);  
-// console.log(arr2);  // [ <5 empty items>, 1, 2, 3, 4, 5 ]
+console.log(arr2);  // [ <5 empty items>, 1, 2, 3, 4, 5 ]
 arr2[0] = 12;
-// console.log(arr2);  // [ 12, <4 empty items>, 1, 2, 3, 4, 5 ]
+console.log(arr2);  // [ 12, <4 empty items>, 1, 2, 3, 4, 5 ]
 
 
 
@@ -21,15 +21,15 @@ myArr.pop()
 
 myArr.unshift(9)  // generally we don't use unshift because it changes all the values indexes
 myArr.shift()
-// console.log(myArr);
+console.log(myArr);
 
-// console.log(myArr.includes(9));  // false
-// console.log(myArr.indexOf(9));  // -1 (because the value doesn't exist)
-// console.log(myArr);
+console.log(myArr.includes(9));  // false
+console.log(myArr.indexOf(9));  // -1 (because the value doesn't exist)
+console.log(myArr);
 
 const newArr = myArr.join()
-// console.log(myArr);  // array
-// console.log(newArr);  // string
+console.log(myArr);  // array
+console.log(newArr);  // string
 
 
 
