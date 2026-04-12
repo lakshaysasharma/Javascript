c09f16a91189ad60af519b6637a7023deb006225
Lakshay Sharma <lakshaysasharma@gmail.com>
2026-04-12 21:58:46 +0530

    variables

---
+++ geeksForGeeks/geeksForGeeks.js
@@ -0,0 +1,34 @@
+// var a = 10
+// console.log(a);
+
+// function test() {
+//     console.log(a);
+// }
+// test()
+
+
+
+// function test() {
+//     let a = 10
+//     function test2() {
+//         let a = 30 
+//         console.log(a)
+//         {
+//             let a = 20
+//             console.log(a);
+//         }
+//     }
+//     test2()
+//     console.log(a);
+// }
+// console.log(a);
+// test();
+
+
+
+const a = 10;
+function test() {
+    // const a = 20;
+    console.log(a);
+}
+test()
