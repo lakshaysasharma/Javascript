cfca289bb6a7db88fb3f1f1a540b4bc0273ff5b3
Lakshay Sharma <lakshaysasharma@gmail.com>
2026-07-22 14:18:23 +0530

    data types

---
+++ Learn from Claude/script_2.js
@@ -0,0 +1,58 @@
+// Datatypes
+
+// Big Int
+let x = 123425464845315148n;
+console.log(x);
+
+let y = BigInt(123541818188188884);
+console.log(y);
+
+
+// Object
+let person = {name: "Lakshay", age: 20};
+console.log(person);
+console.log(person.name);
+
+
+// Array Object
+let cars = ["Volvo", "BMW", "Audi"];
+console.log(cars);
+console.log(cars[1]);
+
+
+// Date Object
+const currentDate = new Date(Date.now());
+console.log(currentDate);
+
+const customDate = new Date("2026");
+console.log(customDate);
+
+
+// Null
+let m = null;
+console.log(m);
+
+let n = undefined;
+console.log(n);
+
+
+// Symbol
+let a = Symbol();
+console.log(a);
+
+
+// Type of
+console.log(typeof("Lakshay"));
+
+
+// String
+let statement_1 = "My name is 'Lakshay'";
+console.log(statement_1);
+
+
+// Exponential notation
+let num_1 = 359e3;
+console.log(num_1);
+
+let num_2 = 34567e-3;
+console.log(num_2);
