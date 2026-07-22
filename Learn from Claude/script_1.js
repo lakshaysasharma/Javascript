31ca36d46c41714413fb141901c7f5988356a1b5
Lakshay Sharma <lakshaysasharma@gmail.com>
2026-07-22 13:48:16 +0530

    let, var, const

---
+++ Learn from Claude/script_1.js
@@ -0,0 +1,52 @@
+let x;
+x = 6;
+console.log(x);
+
+let y = 7;
+console.log(x + y);
+
+console.log((5 + 7) * 2);
+
+const pi = 3.14;
+console.log(pi);
+
+const myName = "Lakshay";
+const _myName = "_Lakshay";
+const $myName = "$Lakshay";
+
+console.log(myName);
+console.log(_myName);
+console.log($myName);
+
+console.log("Lakshay" + " " + "Sharma");
+
+// let my-name = "Lakshay"  // error(variable name)
+
+let age;
+console.log(age);  // undefined
+
+
+const price1 = 6;
+const price2 = 7;
+let total = price1 + price2;
+console.log(total);
+
+let personName = "Lakshay", carName = "Volvo", personAge = 20;
+
+// let personName = "Lakshay", 
+// carName = "Volvo", 
+// age = 20;
+
+let m = "2" + 3 + 5;
+console.log(m);
+
+let n = 2 + 5 + "4";
+console.log(n);
+
+// const area;
+// area = 12;  // error(const must be assigned a value while declaration)
+
+
+var motherName = "Seema";
+var motherName = "Seema Sharma";
+console.log(motherName);
