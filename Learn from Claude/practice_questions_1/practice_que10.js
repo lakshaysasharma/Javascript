3917d4f0fcd11c06b69e95195660a5ec9ec3aa52
Lakshay Sharma <lakshaysasharma@gmail.com>
2026-07-26 03:28:57 +0530

    practice ques

--- Learn from Claude/practice_questions_1/practice_que10.js
+++ Learn from Claude/practice_questions_1/practice_que10.js
@@ -1,7 +1,7 @@
 // Title case — Convert "the quick brown fox" into "The Quick Brown Fox" using .split(" "), a loop, .charAt(), .toUpperCase(), and .slice().
 
 
-let str = "the quick brown fox";
+let str = "many quick brown fox";
 
 console.log(str.slice(0,1));
 
@@ -9,9 +9,11 @@ console.log(str.slice(0,1));
 let words = str.split(" ");
 console.log(words);
 let finalStr = "";
+let firstChar = "";
 
 for(let word of words) {
-    finalStr = finalStr + word.charAt(0).toUpperCase() + " ";
+    firstChar = word.charAt(0).toUpperCase();
+    finalStr = finalStr + " " + firstChar + word.slice(1);
 }
 
 console.log(finalStr);
